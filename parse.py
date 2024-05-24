09d93dcf7431e5cad5f5826b3807ae8e1949a1f5
Viola <jan.viola@ssitech.com>
2024-05-25 01:07:29 +0200

    second repair

--- parse.py
+++ parse.py
@@ -6,21 +6,17 @@ db = client["portabo"]
 collection_hourlytopic = db["topic_za_hodinu"]
 
 
-def parse_mongo_data_by_hourlytopic(data):
-    if 'topic' in data.columns:
-        # Agregace dat podle tématu a výpočet průměrných hodnot pro ostatní sloupce
-        aggregated_data = data.groupby('topic').mean()
-        print(aggregated_data)
-        return aggregated_data
-    else:
-        return pd.DataFrame()
+def parse_mongo_data_by_hourlytopic():
+    # Načtení dat z MongoDB
+    data_list = list(collection_hourlytopic.find())
 
-def categorize_messages(data):
-    if 'topic' in data.columns:
-        categorized_data = data.groupby('topic').apply(lambda x: x)
-        return categorized_data
-    else:
-        return pd.DataFrame()
+    # Příprava datového rámce s explicitním rozbalením vnořench slovníků
+    data_frame = pd.DataFrame(data_list)
+    topics = (data_frame['_id'].apply(lambda x: x['topic']).tolist(), data_frame['count'].tolist())
+
+    # Použití funkce clean_topics na celý DataFrame
+    topic_counts = clean_topics(data_frame)
+
 
 def clean_topics(data):
     def format_topic(topic):
@@ -47,25 +43,8 @@ def clean_topics(data):
     data['cleaned_topic'] = data['_id'].apply(lambda x: format_topic(x['topic']))
 
     # Počítání výskytů jednotlivých témat
-    topic_counts = data['cleaned_topic'].value_counts().to_dict()
+    topic_counts = data.groupby('cleaned_topic')['count'].sum().to_dict()
 
     return topic_counts
 
-# Načtení dat z MongoDB
-data_list = list(collection_hourlytopic.find())
-
-# Příprava datového rámce s explicitním rozbalením vnořených slovníků
-data_frame = pd.DataFrame(data_list)
-#data_frame['hour'] = data_frame[0].apply(lambda x: x['hour'])
-#data_frame['topic'] = data_frame[0].apply(lambda x: x['topic'])
-
-print(data_frame[:5])
-data_frame[:5].to_csv('first_five_items.txt', sep='\t', index=False)
-topics = (data_frame['_id'].apply(lambda x: x['topic']).tolist(), data_frame['count'].tolist())
-
-print(topics)  # Vypíše seznam topiců
-#parse_mongo_data_by_hourlytopic(data_frame)
-
-# Použití funkce clean_topics na celý DataFrame
-topic_counts = clean_topics(data_frame)
-print(topic_counts)  # Vypíše slovník s počty výskytů jednotlivých témat
+parse_mongo_data_by_hourlytopic()
