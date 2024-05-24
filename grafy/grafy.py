4426b7359b6997b7750e9165c2968102ef8d2c61
Marty808s <martybeats.cz@gmail.com>
2024-05-24 23:33:10 +0200

    update grafy

--- grafy/grafy.py
+++ grafy/grafy.py
@@ -6,7 +6,6 @@ import pandas as pd
 import numpy as np
 import os 
 
-
 def scatter_plot(x_categories, y_values, title='Bodový graf'):
     fig = go.Figure(data=[go.Scatter(x=x_categories, y=y_values, mode='markers')])
     fig.update_layout(title=title)
@@ -22,10 +21,13 @@ def pie_chart(labels, values, title='Koláčový graf'):
     fig.update_layout(title=title)
     return fig
 
-
 def time_series_chart(timestamps, throughput_values, title='Časový graf throughputu'):
     fig = go.Figure(data=[go.Scatter(x=timestamps, y=throughput_values)])
     fig.update_layout(title=title, xaxis_title='Čas', yaxis_title='Throughput (jednotky/s)')
     return fig
 
+def bar_chart(x_categories, y_values, title='Sloupcový graf'):
+    fig = go.Figure(data=[go.Bar(x=x_categories, y=y_values)])
+    fig.update_layout(title=title)
+    return fig
 
--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from flask import Flask, render_template
 from grafy import grafy
+import random
 app = Flask(__name__)
+
+
+# Temp data z db
+timestamps = ["2024-05-24T21:14:30", "2024-05-24T21:14:31", "2024-05-24T21:14:32", "2024-05-24T21:14:33", "2024-05-24T21:14:34", "2024-05-24T21:14:35"]
+messages_per_second = [10, 12, 8, 15, 11, 14]
+data_flow_bps = [8000, 9500, 7000, 12000, 8500, 11000]
+
 
 @app.route('/') 
 def domov():
     return render_template('home.html')
 
-"""
-@app.route('/graf')
+
+@app.route('/grafy')
 def graf():
-    fig = grafy.mapa()
-    return render_template('graf.html', plot=fig.to_html(full_html=False))
-"""
+    """
+    x_values = [random.uniform(0, 500) for _ in range(10)]
+    y_values = [random.uniform(0, 100) for _ in range(10)]
+    """
+    # temp data na test
+    fig_scatter = grafy.scatter_plot(timestamps, messages_per_second).to_html(full_html=False)
+    fig_bar = grafy.bar_chart(timestamps, data_flow_bps).to_html(full_html=False)
+    return render_template('grafy.html', plot_scatter=fig_scatter, plot_bar=fig_bar)
 
 
 if __name__ == '__main__':
