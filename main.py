07314d4c065201ebed955f1b2da508574ace118e
Marty808s <martybeats.cz@gmail.com>
2024-05-25 07:32:56 +0200

    update JS

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
 from flask import Flask, render_template
-from grafy import grafy
+from flask_socketio import SocketIO
 import random
+from apscheduler.schedulers.background import BackgroundScheduler
+from grafy import grafy
+
 app = Flask(__name__)
+socketio = SocketIO(app)
 
+# Scheduler setup
+scheduler = BackgroundScheduler()
 
-# Temp data z db
 sensors = ["sensor1", "sensor2", "sensor3", "sensor4", "sensor5", "sensor6"]
-events_per_sensor = [random.randint(1, 100) for _ in sensors]
-
-# temp data i pro láďu
 timestamps = ["2024-05-24T21:14:30", "2024-05-24T21:14:31", "2024-05-24T21:14:32", "2024-05-24T21:14:33", "2024-05-24T21:14:34", "2024-05-24T21:14:35"]
-messages_per_second = [10, 12, 8, 15, 11, 14]
-data_flow_bps = [8000, 9500, 7000, 12000, 8500, 11000]
 
+def update_data():
+    print('Update...')
+    global events_per_sensor, messages_per_second, data_flow_bps
+    events_per_sensor = [random.randint(1, 100) for _ in sensors]
+    messages_per_second = [random.randint(5, 20) for _ in range(6)]
+    data_flow_bps = [random.randint(5000, 15000) for _ in range(6)]
+    
+    print(events_per_sensor)
+    print(messages_per_second)
+    print(data_flow_bps)
 
-@app.route('/') 
-def domov():
-    fig_in_time_s = grafy.time_series_chart(timestamps, messages_per_second,'Časová řada [s]').to_html(full_html=False)
-    fig_in_time_h = grafy.time_series_chart(timestamps, messages_per_second,'Časová řada [h]').to_html(full_html=False)
-    fig_bar_sensor = grafy.bar_chart(sensors, events_per_sensor,'Senzory').to_html(full_html=False)
-    return render_template('home.html', plot_in_time_s=fig_in_time_s, plot_in_time_h=fig_in_time_h, plot_bar_sensor=fig_bar_sensor)
+    # generování HTML grafů
+    plot_in_time_s = grafy.scatter_plot(timestamps, messages_per_second, 'Messages Per Second').to_html(full_html=False)
+    plot_in_time_h = grafy.area_chart(timestamps, data_flow_bps, 'Data Flow in bps').to_html(full_html=False)
+    plot_bar_sensor = grafy.bar_chart(sensors, events_per_sensor, 'Events Per Sensor').to_html(full_html=False)
+    
+    # odeslání HTML grafů
+    socketio.emit('data_update', {
+        'plot_in_time_s': plot_in_time_s,
+        'plot_in_time_h': plot_in_time_h,
+        'plot_bar_sensor': plot_bar_sensor
+    })
 
+    return plot_in_time_s, plot_in_time_h, plot_bar_sensor
 
+scheduler.add_job(update_data, 'interval', seconds=5)
+scheduler.start()
+
+@app.route('/')
+def home():
+    plot_in_time_s, plot_in_time_h, plot_bar_sensor = update_data()
+    return render_template('home.html', plot_in_time_s=plot_in_time_s, plot_in_time_h=plot_in_time_h, plot_bar_sensor=plot_bar_sensor)
+
+# stránka na grafy - zaměřené pro jiné values
 @app.route('/grafy')
 def graf():
     # temp data na test
@@ -29,6 +54,5 @@ def graf():
     fig_bar = grafy.bar_chart(timestamps, data_flow_bps,'Sloupcov graf').to_html(full_html=False)
     return render_template('grafy.html', plot_scatter=fig_scatter, plot_bar=fig_bar)
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
