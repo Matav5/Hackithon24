80acb1d73a3e5ab4a388ae28e5cd57075be76680
Matav5 <verner.matej@gmail.com>
2024-05-25 13:12:27 +0200

    Pipeliny ve zpracování a nedostupnost dat.Wohooo

--- data/zpracovani.py
+++ data/zpracovani.py
@@ -3,20 +3,22 @@
 import pandas as pd
 import pymongo as pm
 from datetime import datetime, timedelta,timezone
-import pipeliny
+
+
 client = pm.MongoClient("mongodb://matav:Nevim123@167.86.71.184:27017/")
 db = client["portabo"]
 collection = db["portabo"]
+throughput_collection = db["throughput"]
 
     
 def get_topics():
-    data_list = list(collection.aggregate(pipeliny.branchesPipeline()))
+    data_list = list(collection.aggregate(topicPipeline()))
     data_list = [item['_id'] for item in data_list]
     return data_list
  
 def get_topic(nazev,od=None ,do=None):
     ls =list()
-    for item in pipeliny.topicPipeline(nazev):
+    for item in topicPipeline(nazev):
         ls.append(item)
     for item in time_filter(od, do):
         ls.append(item)
@@ -42,20 +44,168 @@ def time_filter(od, do=None):
     ]
     return pipeline
 
-
-def get_cetnosti(od=None, do=None):
+def get_cetnosti(od=None,do=None):
     ls = list()
     for item in time_filter(od, do):
         ls.append(item)
-    for item in pipeliny.cetnostiPipeline():
+    for item in cetnostiPipeline():
         ls.append(item)    
     data_list = list(collection.aggregate(ls))
     df = pd.DataFrame(data_list)
     df.rename(columns={'_id': 'nazev'}, inplace=True)
     return df
-#vycistit zdokumenetovat, udělat četnosti, devops
+
+def get_throughput(od=None,do=None):
+    ls = list()
+    
+    for item in time_filter(od, do):
+        ls.append(item)
+
+    data_list = list(throughput_collection.aggregate(ls))
+    print(data_list)
+    df = pd.DataFrame(data_list, columns=['timestamp', 'messages_per_second', 'throughput_per_second']) 
+    print(df.head())
+    return df
+
+
+
+  
+
+regex_then_dict = {
+    "^/ttndata": "/ttndata",
+    "^/vodomery": "/vodomery",
+    "^/Bilina": "/Bilina",
+    "^/senzory": "/senzory",
+    "^/mve": "/mve",
+    "^/voda": "/voda",
+    "^/udp1881": "/udp1881"
+}
+default_pipeline= []
+
+
+branches = [
+    {"case": {"$regexMatch": {"input": "$topic", "regex": regex}}, "then": then} for regex, then in regex_then_dict.items()
+]
+
+def branchesPipeline():
+    pipeline = [
+        {
+            "$set": {
+                "general_topic": {
+                    "$switch": {
+                        "branches": branches,
+                        "default": "$topic"
+                    }
+                }
+            }
+        },
+        {
+            "$group": {
+                "_id": "$general_topic"
+            }
+        },
+        {
+            "$sort": {"_id": 1}
+        }
+    ]
+    return pipeline
+
+
+def topicPipeline(nazev):
+    topicPipeline = [
+        {
+            "$set": {
+            "general_topic": {
+                "$switch": {
+                "branches": branches,
+                "default": "$topic"
+                }
+            }
+            }
+        },
+        {
+            "$match": { "general_topic": nazev }
+        },
+        {
+            "$sort": { "_id": 1 }
+        }
+    ]
+        
+    return topicPipeline
+
+
+
+def cetnostiPipeline():
+    pipe = [
+    {
+        '$project': {
+            'payload': 1
+        }
+    }, {
+        '$addFields': {
+            'payloadFields': {
+                '$cond': {
+                    'if': {
+                        '$eq': [
+                            {
+                                '$type': '$payload'
+                            }, 'object'
+                        ]
+                    }, 
+                    'then': {
+                        '$objectToArray': '$payload'
+                    }, 
+                    'else': {
+                        '$map': {
+                            'input': {
+                                '$cond': {
+                                    'if': {
+                                        '$isArray': '$payload'
+                                    }, 
+                                    'then': '$payload', 
+                                    'else': []
+                                }
+                            }, 
+                            'as': 'item', 
+                            'in': {
+                                '$objectToArray': '$$item'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }, {
+        '$unwind': {
+            'path': '$payloadFields', 
+            'preserveNullAndEmptyArrays': True
+        }
+    }, {
+        '$unwind': {
+            'path': '$payloadFields', 
+            'preserveNullAndEmptyArrays': True
+        }
+    }, {
+        '$group': {
+            '_id': '$payloadFields.k', 
+            'count': {
+                '$sum': 1
+            }
+        }
+    }, {
+        '$sort': {
+            'count': -1
+        }
+    }
+]
+    return pipe
+
 
 if __name__ == "__main__":
-    print(get_topics());
-    print(get_cetnosti());
+  #  print(get_topics());
+   # print(get_cetnosti());
+  #  print(get_throughput(datetime(2024,5,24,22),datetime(2024,5,24,22,5)));
+    print(get_topic("/Bilina",datetime(2024,5,24,22),datetime(2024,5,24,22,5)));
   #  print(get_topic("/Bilina",datetime(2024,5,24,22),datetime(2024,5,24,22,5)));
+  
+  
--- main.py
+++ main.py
@@ -1,48 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from flask_socketio import SocketIO
 import random
 from apscheduler.schedulers.background import BackgroundScheduler
 from grafy import grafy
+import data.zpracovani as zprac
+from datetime import timedelta,timezone,datetime
+import sys
 
 app = Flask(__name__)
 
 sensors = ["sensor1", "sensor2", "sensor3", "sensor4", "sensor5", "sensor6"]
 timestamps = ["2024-05-24T21:14:30", "2024-05-24T21:14:31", "2024-05-24T21:14:32", "2024-05-24T21:14:33", "2024-05-24T21:14:34", "2024-05-24T21:14:35"]
 
-def update_data():
+def update_data(od=None):
     print('Update...')
-    global events_per_sensor, messages_per_second, data_flow_bps
-    events_per_sensor = [random.randint(1, 100) for _ in sensors]
-    messages_per_second = [random.randint(5, 20) for _ in range(6)]
-    data_flow_bps = [random.randint(5000, 15000) for _ in range(6)]
+    if od is None:
+        od = datetime.now(tz=timezone.utc) - timedelta(minutes=25)
+
+    throughput = zprac.get_throughput(od)
     
-    print(events_per_sensor)
-    print(messages_per_second)
-    print(data_flow_bps)
+    events_per_sensor = throughput[["timestamp", "messages_per_second"]].copy()
+    messages_per_second = throughput[["timestamp", "messages_per_second"]].copy()
+    data_flow_bps =throughput[["timestamp", "throughput_per_second"]].copy() 
+    
+    print(events_per_sensor.head())
+    print(messages_per_second.head())
+    print(data_flow_bps.head())
+   
 
     # generování HTML grafů
-    plot_in_time_s = grafy.scatter_plot(timestamps, messages_per_second, 'Messages Per Second').to_html(full_html=False)
-    plot_in_time_h = grafy.area_chart(timestamps, data_flow_bps, 'Data Flow in bps').to_html(full_html=False)
-    plot_bar_sensor = grafy.bar_chart(sensors, events_per_sensor, 'Events Per Sensor').to_html(full_html=False)
-    
 
-    return plot_in_time_s, plot_in_time_h, plot_bar_sensor
 
-@app.route('/api/data')
+    return messages_per_second, data_flow_bps, events_per_sensor
+
+@app.route('/api/data', methods=['GET'])
 def api_data():
-    # Získání aktuálních dat
-    plot_in_time_s, plot_in_time_h, plot_bar_sensor = update_data()
+    date = request.args.get('date')
+
+    messages_per_second, data_flow_bps, events_per_sensor = update_data(date)
+    
     return {
-        'plot_in_time_s': plot_in_time_s,
-        'plot_in_time_h': plot_in_time_h,
-        'plot_bar_sensor': plot_bar_sensor
+        'plot_in_time_s': messages_per_second.to_json(),
+        'plot_in_time_h': data_flow_bps.to_json(),
+        'plot_bar_sensor': events_per_sensor.to_json(),
+        'datum': datetime.now(tz=timezone.utc).isoformat()
     }
-
-
 @app.route('/')
 def home():
-    plot_in_time_s, plot_in_time_h, plot_bar_sensor = update_data()
-    return render_template('home.html', plot_in_time_s=plot_in_time_s, plot_in_time_h=plot_in_time_h, plot_bar_sensor=plot_bar_sensor)
+    
+    messages_per_second, data_flow_bps, events_per_sensor = update_data()
+    
+    
+    plot_in_time_s = grafy.scatter_plot(messages_per_second["timestamp"], messages_per_second["messages_per_second"], 'Messages Per Second').to_html(full_html=False)
+    plot_in_time_h = grafy.area_chart(data_flow_bps["timestamp"], data_flow_bps["throughput_per_second"], 'Data Flow in bps').to_html(full_html=False)
+    plot_bar_sensor = grafy.bar_chart(events_per_sensor["timestamp"],events_per_sensor["messages_per_second"], 'Events Per Sensor').to_html(full_html=False)
+    
+    return render_template('home.html', plot_in_time_s=plot_in_time_s, plot_in_time_h=plot_in_time_h, plot_bar_sensor=plot_bar_sensor,datum=datetime.now(tz=timezone.utc))
 
 
 # stránka na grafy - zaměřené pro jiné values
@@ -53,6 +66,6 @@ def graf():
     fig_bar = grafy.bar_chart(timestamps, data_flow_bps,'Sloupcov graf').to_html(full_html=False)
     return render_template('grafy.html', plot_scatter=fig_scatter, plot_bar=fig_bar)
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+  
